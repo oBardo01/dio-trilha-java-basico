599485f2f50af7a9ed83a47d49509021ba7d647e
Pedro Abreu <opedroabreu21@gmail.com>
2025-04-14 14:31:50 -0300

    Aula Operadores e Variáveis concluidas

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,18 @@
+public class Operadores {
+     public static void main(String[] args) {
+        int numero = -5;
+        System.out.println(- numero);
+
+        int a = 9, b = 8;
+        String resultado = a < b ? "true" : "false";
+        System.out.println(resultado);
+
+        boolean igualOuNao = a == b;
+        System.out.println(igualOuNao);
+
+
+        String nomeUm = "Pedro";
+        String nomeDois = "Pedro";
+        System.out.println(nomeUm.equals(nomeDois));
+     }
+}
--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,16 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        double salarioMinimal = 2500.00;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 5;
+
+        numero = 10;
+
+        final double VALOR_DE_PI = 3.14;
     }
 }
